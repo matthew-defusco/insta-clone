0690b254b17f1dad7f976c88daa23aa017466a0b
Matt <matthew.defusco@gmail.com>
2024-06-08 17:13:42 -0400

    trying to set up for prod deploy

--- client/src/components/RequireAuth.js
+++ client/src/components/RequireAuth.js
@@ -1,12 +1,13 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../context/auth";
 import UserLayout from "../routes/UserLayout/UserLayout";
 
 export const RequireAuth = () => {
   const { user } = useAuth();
+  const location = useLocation();
 
   if (!user) {
-    return <Navigate to="/" replace />;
+    return <Navigate to="/" replace state={{ from: location }} />;
   }
 
   return (
